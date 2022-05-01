76c36b0d911d317c314adcd4a41941f34c0f0729
forhadmia1 <forhad8613@gmail.com>
2022-05-01 22:38:29 +0600

    database connect with server

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wlt58.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('db connected')
+    // perform actions on the collection object
+    client.close();
+});
 
 
 app.get('/', (req, res) => {
@@ -10,3 +29,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('listening port', port)
 })
+
+//root api
+// https://protected-atoll-86406.herokuapp.com/
